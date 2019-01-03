511c4df013894dab749196103337b883a38217d4
Adam Kiss <i.am.adam.kiss+git@gmail.com>
2019-01-03 15:12:15 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/* global document */
+
+import {h, app} from 'hyperapp'
+import './index.css'
+import App from './app'
+
+const state = {}
+
+const actions = {}
+
+app(state, actions, () => <App />, document.body)
